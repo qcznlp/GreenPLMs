c69726ffc45791ae35fef6bd7903fb09fb7f31ac
Steve Zeng <stevezenguom@gmail.com>
2021-11-01 17:21:38 +0800

    Enter

--- idea_1/seg_and_trans.py
+++ idea_1/seg_and_trans.py
@@ -1,21 +1,15 @@
 import jieba
 import os
-from facebook_dict_cleaning import generate_dict_mapping_from_dictionary_zh_en
 from transformers import BertTokenizer,BertModel
-from word2word import Word2word
+from tokenization_wobert import WoBertTokenizer
 
-jieba.enable_paddle()
-
-generated_dict = generate_dict_mapping_from_dictionary_zh_en("zh","en")
-
-def cut_words_and_transfer(entrance_string):
-    zh2en = Word2word("zh_cn","en")
-    word_list = jieba.cut(entrance_string,use_paddle=True)
+def cut_words_and_transfer(entrance_string,bilingual_dict):
+    word_list = jieba.cut(entrance_string)
     trans_list = []
     unknown_list = []
     for word in word_list:
         try:
-            trans_word = zh2en(word)[0]
+            trans_word = bilingual_dict[word]
             trans_list.append(trans_word)
         except KeyError:
             unknown_list.append(word)
@@ -23,11 +17,16 @@ def cut_words_and_transfer(entrance_string):
     trans_string = " ".join(trans_list)
     return trans_string,unknown_list
 
-def merge_UNK_into_models(unknown_word_list,model_path,new_path):
+def merge_UNK_into_models(unknown_word_list,model_path):
     model = BertModel.from_pretrained(model_path)
-    tokenizer = BertTokenizer.from_pretrained(model_path)
+    tokenizer = WoBertTokenizer("E:\\Steve_Zeng_Related\\YLab\\Translation_BERT_project\\code_project\\data\\BERT-base-uncased-trans-processed.txt")
 
     tokenizer.add_tokens(unknown_word_list)
     model.resize_token_embeddings(len(tokenizer))
-    model.save_pretrained(new_path)
-    tokenizer.save_pretrained(new_path)
+
+    return model,tokenizer
+
+"""
+m,t = merge_UNK_into_models(["mscsc"],"E:\\Steve_Zeng_Related\\YLab\\Translation_BERT_project\\code_project\\BERT\\bert-base-uncased-trans")
+print(t.tokenize("我喜欢威廉姆斯"))
+"""
